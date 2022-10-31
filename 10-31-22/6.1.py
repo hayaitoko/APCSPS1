ca763993db60be613e973bc7ca53d68bfbb84398
hayaitoko <95260644+hayaitoko@users.noreply.github.com>
2022-10-31 09:07:18 -0700

    Add files via upload

---
+++ 10-31-22/6.1.py
@@ -0,0 +1,7 @@
+s1 = "High School"
+print(s1[ 2 : -4])  
+print(s1[ 1:5])
+ 
+print(s1[ :5])
+print(s1[ 1:])
+print(s1[ : ])
---
+++ 10-31-22/6.1a.py
@@ -0,0 +1,7 @@
+s1  = "I go to High School"
+print(s1[-1])   
+print(s1[0])
+print(s1[-6])   
+print(s1[1])
+print(s1[-3])   
+print(s1[3])
